e3ed843f27082efa8233ee43335cffa810e67e35
NavodYasara <111131809+NavodYasara@users.noreply.github.com>
2024-03-23 19:54:02 +0530

    Add react-router-dom and update dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import Home from './Pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Register from "./Pages/Register";
 import Navbar from './Components/Navbar/Navbar'
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,31 @@
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+// import img1 from '/Users/USER/Desktop/serene-care-solution/frontend/Serene-care-frontend/src/Resources/img1.png'; // Corrected import path
+// import { Link } from 'react-router-dom';
 
 function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        {/* <Navbar.Brand href="#home">Serene Care</Navbar.Brand> */}
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={img1} // Use the imported image
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+        </Navbar.Brand>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <img src="" alt="" />
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Our Services</Nav.Link>
-            <Nav.Link href="#link">Contact Us</Nav.Link> 
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+            <Nav.Link href="/src/Pages/Login.jsx">Login</Nav.Link> 
           </Nav>
         </Navbar.Collapse>
       </Container>
