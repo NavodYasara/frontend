e5cf9b133fca01b006b05d4cd53ca5daf85c8b0f
NavodYasara <111131809+NavodYasara@users.noreply.github.com>
2024-03-27 17:54:54 +0530

    Refactor login and register forms to use react-bootstrap components

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,43 +1,39 @@
 import React from 'react';
-import {
-  MDBBtn,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBInput,
-  MDBCheckbox,
-  MDBIcon
-} from 'mdb-react-ui-kit';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 
 function FormDisabledInputExample() {
   return (
-    <MDBContainer className="d-flex justify-content-center align-items-center vh-100">
-      <MDBCol md='6'>
-        <MDBCard className='my-5'>
-          <MDBCardBody className='p-5'>
-            <MDBRow>
-              <MDBCol col='6'>
-                <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
-              </MDBCol>
-              <MDBCol col='6'>
-                <MDBInput wrapperClass='mb-4' label='Last name' id='form1' type='text'/>
-              </MDBCol>
-            </MDBRow>
-            <MDBInput wrapperClass='mb-4' label='Email' id='form1' type='email'/>
-            <MDBInput wrapperClass='mb-4' label='Password' id='form1' type='password'/>
-
-            {/* <div className='d-flex justify-content-center mb-4'>
-              <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
-            </div> */}
-            
-            <MDBBtn className='w-100 mb-4' size='md'>sign up</MDBBtn>
-            
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-    </MDBContainer>
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Col md='6'>
+        <Card className='my-5'>
+          <Card.Body className='p-5'>
+            <Row>
+              <Col md='6'>
+                <Form.Group className='mb-4'>
+                  <Form.Label>First name</Form.Label>
+                  <Form.Control type='text' placeholder='Enter your first name' />
+                </Form.Group>
+              </Col>
+              <Col md='6'>
+                <Form.Group className='mb-4'>
+                  <Form.Label>Last name</Form.Label>
+                  <Form.Control type='text' placeholder='Enter your last name' />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group className='mb-4'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control type='email' placeholder='Enter your email' />
+            </Form.Group>
+            <Form.Group className='mb-4'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' placeholder='Enter your password' />
+            </Form.Group>
+            <Button className='w-100 mb-4' size='md' variant='primary'>Sign Up</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Container>
   );
 }
 
