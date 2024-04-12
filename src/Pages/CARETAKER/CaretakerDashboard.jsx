50ef0c63f0cdb8e21a26874775deebfe056552ab
NavodYasara <111131809+NavodYasara@users.noreply.github.com>
2024-04-12 16:36:00 +0530

    Update CaretakerDashboard component styling and layout

--- src/Pages/CARETAKER/CaretakerDashboard.jsx
+++ src/Pages/CARETAKER/CaretakerDashboard.jsx
@@ -1,7 +1,9 @@
  import React from 'react';
 import Sidebar from '../../Components/Sidebar'
-import { Container, Table } from 'react-bootstrap';
+import { Container, Table} from 'react-bootstrap';
 import Avatar from '@mui/material/Avatar';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 
 function CaretakerDashboard() {
@@ -12,9 +14,9 @@ function CaretakerDashboard() {
             </div>
 
             <div style={{marginLeft:'280px'}}>
-                <Container fluid className="vh-100 d-flex ">
-                    <div className="flex-grow-1 ">
-                        <div className="d-flex justify-content-center align-items-center h-100" >
+                <Container fluid className="vh-100 d-flex " style={{width:'100%'}}>
+                    <div className="flex-grow-2">
+                        <div className="d-flex justify-content-center align-items-center h-100 " >
                             <div className="text-center p-5 shadow rounded">
                                 <Avatar alt="Profile Picture" src="" sx={{ width: 100, height: 100, marginBottom: 20 }} />
                                 <Table borderless className="invisible-table">
@@ -38,6 +40,21 @@ function CaretakerDashboard() {
                                         <tr>
                                             <td className="fw-bold">Contact Number:</td>
                                             <td>123-456-7890</td>
+                                        </tr>
+                                        <tr>
+                                            
+                                            <Dropdown>
+                                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                                    Dropdown Button
+                                                </Dropdown.Toggle>
+
+                                                <Dropdown.Menu>
+                                                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                                </Dropdown.Menu>
+                                            </Dropdown>
+
                                         </tr>
                                     </tbody>
                                 </Table>
