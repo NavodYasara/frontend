f39d81eb2ce944903216db735214d8993c57708f
NavodYasara <111131809+NavodYasara@users.noreply.github.com>
2024-05-01 17:17:43 +0530

    Refactor Register.jsx component and add ManagerDashboard component

--- src/Pages/CARETAKER/CaretakerDashboard.jsx
+++ src/Pages/CARETAKER/CaretakerDashboard.jsx
@@ -1,79 +1,172 @@
+import React, { useState } from "react";
+import Sidebar from "../../Components/Sidebar2";
+import { Container, Table } from "react-bootstrap";
+import Button from "@mui/material/Button";
+import Dropdown from "react-bootstrap/Dropdown";
 
-import React from 'react';
-import Sidebar from '../../Components/Sidebar2'
-import { Container, Table} from 'react-bootstrap';
-// import Avatar from '@mui/material/Avatar';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { useState } from 'react';
 
 function CaretakerDashboard() {
-    const [selectedCategory, setSelectedCategory] = useState(''); // Define setSelectedCategory using useState hook
+  
+  const [editMode, setEditMode] = useState(false);
+  const [profileData, setProfileData] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    address: "123 Main St, City",
+    contactNumber: "123-456-7890",
+    selectedCategory: "",
+    requirements: "Bla bla bla",
+  });
+  const [selectedCategory, setSelectedCategory] = useState("");
 
-    return (
-        <div style={{ display: 'flex' }}>
-            <div>
-                <Sidebar/>
+  const handleEditProfile = () => {
+    setEditMode(true);
+  };
+
+  const handleCancel = () => {
+    setEditMode(false);
+  };
+
+  const handleSave = () => {
+    // Here you can implement the backend logic to save the updated profile data
+    setEditMode(false);
+    // Send a request to your backend to save profileData
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProfileData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div style={{ display: "flex" }}>
+      <div>
+        <Sidebar />
+      </div>
+
+      <div
+        fluid
+        className="vh-100 d-flex "
+        style={{ width: "100%", marginTop: "100px" }}
+      >
+      
+        <Container>
+          <div className="flex-grow-2 " >
+            <div className="d-flex justify-content-center align-items-center h-100">
+              <div
+                className="text-center p-4 shadow rounded"
+                style={{ width: "50vw" }}
+              >
+                <Table borderless className="invisible-table">
+                  <tbody>
+                    <tr>
+                      <td className="fw-bold">First Name:</td>
+                      <td>{editMode ? <input type="text" name="firstName" value={profileData.firstName} onChange={handleChange} /> : profileData.firstName}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">Last Name:</td>
+                      <td>{editMode ? <input type="text" name="lastName" value={profileData.lastName} onChange={handleChange} /> : profileData.lastName}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">Age:</td>
+                      <td>{editMode ? <input type="number" name="age" value={profileData.age} onChange={handleChange} /> : profileData.age}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">Address:</td>
+                      <td>{editMode ? <input type="text" name="address" value={profileData.address} onChange={handleChange} /> : profileData.address}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">Contact Number:</td>
+                      <td>{editMode ? <input type="text" name="contactNumber" value={profileData.contactNumber} onChange={handleChange} /> : profileData.contactNumber}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">
+                        <Dropdown>
+                          <Dropdown.Toggle
+                            variant="success"
+                            id="dropdown-basic"
+                          >
+                            Disable Category
+                          </Dropdown.Toggle>
+
+                          <Dropdown.Menu>
+                            <Dropdown.Item
+                              onClick={() =>
+                                setSelectedCategory("Mental disorders")
+                              }
+                            >
+                              Mental disorders
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                              onClick={() =>
+                                setSelectedCategory("Physical Disability")
+                              }
+                            >
+                              Physical Disability
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                              onClick={() => setSelectedCategory("Eldering")}
+                            >
+                              Eldering
+                            </Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Dropdown>
+                      </td>
+                      <td>{selectedCategory}</td>
+                    </tr>
+                    <tr>
+                      <td className="fw-bold">Your requirements:</td>
+                      <td>{editMode ? <input type="text" name="requirements" value={profileData.requirements} onChange={handleChange} /> : profileData.requirements}</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
             </div>
+          </div>
 
-            <div fluid className="vh-100 d-flex " style={{width:'100%', marginTop: '100px'}}>
-                <Container >
-                    <div className="flex-grow-2">
-                        <div className="d-flex justify-content-center align-items-center h-100 " >
-                            <div className="text-center p-4 shadow rounded" style={{ width: '50vw' }}>
-                                {/* <Avatar alt="Profile Picture" src="" sx={{ width: 100, height: 100, marginBottom: 20 }} /> */}
-                                <Table borderless className="invisible-table">
-                                    <tbody>
-                                        <tr>
-                                            <td className="fw-bold">First Name:</td>
-                                            <td>John</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold">Last Name:</td>
-                                            <td>Doe</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold">Age:</td>
-                                            <td>30</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold">Address:</td>
-                                            <td>123 Main St, City</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold">Contact Number:</td>
-                                            <td>123-456-7890</td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold">
-                                                <Dropdown>
-                                                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                                        Disble Catagory
-                                                    </Dropdown.Toggle>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "20px",
+            }}
+          >
+            {editMode ? (
+              <>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ marginRight: "10px" }}
+                  onClick={handleSave}
+                >
+                  Ok
+                </Button>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </Button>
+              </>
+            ) : (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleEditProfile}
+              >
+                Edit Profile
+              </Button>
+            )}
+          </div>
+        </Container>
 
-                                                    <Dropdown.Menu>
-                                                        <Dropdown.Item onClick={() => setSelectedCategory('Mental disorders')}>Mental disorders</Dropdown.Item>
-                                                        <Dropdown.Item onClick={() => setSelectedCategory('Physical Disability')}>Physical Disability</Dropdown.Item>
-                                                        <Dropdown.Item onClick={() => setSelectedCategory('Eldering')}>Eldering</Dropdown.Item>
-                                                    </Dropdown.Menu>
-                                                </Dropdown>
-                                            </td>
-                                            <td>
-                                                {selectedCategory}
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td className="fw-bold">Your requirements:</td>
-                                            <td>Bla bla bla</td>
-                                        </tr>
-                                    </tbody>
-                                </Table>
-                            </div>
-                        </div>
-                    </div>
-                </Container>
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default CaretakerDashboard;
