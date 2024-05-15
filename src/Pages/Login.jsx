0165f6fcfe077a86c6778f5341130df88786a53a
NavodYasara <111131809+NavodYasara@users.noreply.github.com>
2024-05-15 12:16:24 +0530

    15/05 - Register and Login is Done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
-import { Container, Col, Card, Form, Button, Dropdown } from "react-bootstrap";
-import { Row } from "react-bootstrap";
+import { Container, Col, Card, Form, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Navbar from "../Components/Navbar/Navbar";
-// import { UserContext } from "./UserContext";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [userType, setUserType] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
@@ -20,11 +17,10 @@ function Login() {
       const response = await axios.post("/server/login", {
         username,
         password,
-        usertype: userType,
       });
 
       if (response.status === 200) {
-        setUserType(userType);
+        const userType = response.data.userType; 
 
         switch (userType) {
           case "caretaker":
@@ -41,7 +37,7 @@ function Login() {
             break;
         }
       } else {
-        setError("Invalid login credentials or user type.");
+        setError("Invalid login credentials.");
       }
     } catch (error) {
       console.error(error);
@@ -62,35 +58,7 @@ function Login() {
                 <div className="title mb-4 text-center">
                   <h3>Login</h3>
                 </div>
-                <Row className="justify-content-center mb-4">
-                  <Col md="6">
-                    <Form.Group>
-                      <Dropdown className="d-flex justify-content-center">
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                          {userType || "Select User Type"}
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                          <Dropdown.Item
-                            onClick={() => setUserType("caretaker")}
-                          >
-                            Caretaker
-                          </Dropdown.Item>
-                          <Dropdown.Item
-                            onClick={() => setUserType("caregiver")}
-                          >
-                            Caregiver
-                          </Dropdown.Item>
-                          <Dropdown.Item onClick={() => setUserType("admin")}>
-                            Admin
-                          </Dropdown.Item>
-                          <Dropdown.Item onClick={() => setUserType("manager")}>
-                            Manager
-                          </Dropdown.Item>
-                        </Dropdown.Menu>
-                      </Dropdown>
-                    </Form.Group>
-                  </Col>
-                </Row>
+
                 <Form.Group className="mb-4">
                   <Form.Label>User Name</Form.Label>
                   <Form.Control
@@ -116,7 +84,7 @@ function Login() {
                   variant="primary"
                   onClick={handleLogin}
                 >
-                  {userType ? `Sign in as ${userType}` : "Sign In"}
+                  Sign In
                 </Button>
               </Card.Body>
             </Card>
@@ -128,3 +96,106 @@ function Login() {
 }
 
 export default Login;
+
+
+// import React, { useState } from "react";
+// import { Container, Col, Card, Form, Button } from "react-bootstrap";
+// import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import Navbar from "../Components/Navbar/Navbar";
+
+
+// function Login() {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [userType, setUserType] = useState("");
+//   const [error, setError] = useState("");
+//   const navigate = useNavigate();
+
+//   const handleLogin = async (event) => {
+//   event.preventDefault();
+
+//   try {
+//     const response = await axios.post("/server/login", {
+//       username,
+//       password,
+//     });
+
+//     if (response.status === 200) {
+//       const userType = response.data.userType;
+
+//       switch (userType) {
+//         case "caretaker":
+//           navigate("/CaretakerDashboard");
+//           break;
+//         case "caregiver":
+//           navigate("/CaregiverDashboard");
+//           break;
+//         case "manager":
+//           navigate("/ManagerDashboard");
+//           break;
+//         default:
+//           setError("Invalid user type.");
+//           break;
+//       }
+//     } else {
+//       setError("Invalid login credentials.");
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     setError("An error occurred while logging in.");
+//   }
+// };
+
+
+//   return (
+//     <>
+//       <div>
+//         <Navbar />
+//       </div>
+//       <div>
+//         <Container className="d-flex justify-content-center align-items-center vh-100">
+//           <Col md="6">
+//             <Card className="my-5">
+//               <Card.Body className="p-5">
+//                 <div className="title mb-4 text-center">
+//                   <h3>Login</h3>
+//                 </div>
+
+//                 <Form.Group className="mb-4">
+//                   <Form.Label>User Name</Form.Label>
+//                   <Form.Control
+//                     type="text"
+//                     placeholder="Enter your username"
+//                     value={username}
+//                     onChange={(e) => setUsername(e.target.value)}
+//                   />
+//                 </Form.Group>
+//                 <Form.Group className="mb-4">
+//                   <Form.Label>Password</Form.Label>
+//                   <Form.Control
+//                     type="password"
+//                     placeholder="Enter your password"
+//                     value={password}
+//                     onChange={(e) => setPassword(e.target.value)}
+//                   />
+//                 </Form.Group>
+//                 {error && <div className="text-danger mb-3">{error}</div>}
+//                 <Button
+//                   className="btn-login w-100 mb-4"
+//                   size="md"
+//                   variant="primary"
+//                   onClick={handleLogin}
+//                 >
+//                   {userType ? `Sign in as ${userType}` : "Sign In"}
+//                 </Button>
+//               </Card.Body>
+//             </Card>
+//           </Col>
+//         </Container>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Login;
