f0e675c16e93db1e6b0c9764e4ce47d541de6608
NavodYasara <111131809+NavodYasara@users.noreply.github.com>
2024-05-17 19:02:40 +0530

    chore: Remove unused CSS file in ManagerDashboard.jsx

--- src/Pages/MANAGER/ManagerDashboard.jsx
+++ src/Pages/MANAGER/ManagerDashboard.jsx
@@ -10,7 +10,7 @@ import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
-import "./ManagerDashboard.css";
+
 
 const ManagerDashboard = () => {
   const [caretakers, setCaretakers] = useState([]);
