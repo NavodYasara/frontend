7ab53dfa0caf34b40296c2a84b7a2760d05f4b0a
NavodYasara <111131809+NavodYasara@users.noreply.github.com>
2024-05-20 20:00:23 +0530

    20/05 - update exeptions in register form

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+import { Container, Col, Card, Form, Button } from "react-bootstrap";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
@@ -8,8 +8,9 @@ const Register = () => {
   const [form, setForm] = useState({
     firstName: "",
     lastName: "",
+    userName: "",
     password: "",
-    confpassword: "",
+    confPassword: "",
     mobileNo: "",
     dob: "",
     gender: "",
@@ -18,6 +19,7 @@ const Register = () => {
     usertype: "caretaker",
   });
 
+  const [error, setError] = useState(null); // State to hold error messages
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -30,10 +32,8 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (
-      form.firstName === "" ||
-      form.lastName === "" ||
+      form.userName === "" ||
       form.password === "" ||
       form.confPassword === "" ||
       form.mobileNo === "" ||
@@ -41,81 +41,42 @@ const Register = () => {
       form.address === "" ||
       form.category === ""
     ) {
-      alert("Please fill all the fields!");
+      setError("Please fill all the fields!");
       return;
-    // } else if (form.mobileNo.match(/^[0-9]{10}$/) === null) {
-    //   alert("Please enter a valid mobile number!");
-    //   return;
-    // } else if (form.password !== form.confPassword) {
-    //   alert("Password and confirm password do not match!");
-    //   return;
+    } else if (form.mobileNo.match(/^[0-9]{10}$/) === null) {
+      setError("Please enter a valid mobile number!");
+      return;
+    } else if (form.password !== form.confPassword) {
+      setError("Password and confirm password do not match!");
+      return;
     }
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/server/registercaretaker",form
+        "http://localhost:5000/api/registercaretaker",
+        form
       );
       console.log(response.data);
       navigate("/Login");
     } catch (error) {
       if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
         if (error.response.status === 400) {
-          alert("Bad request. Please check your inputs and try again.");
+          setError("Bad request. Please check your inputs and try again.");
         } else if (error.response.status === 401) {
-          alert("Unauthorized. Please check your credentials and try again.");
+          setError("Unauthorized. Please check your credentials and try again.");
+        } else if (error.response.status === 409) {
+          setError("Username already exists. Please choose a different username.");
         } else {
-          alert("An error occurred. Please try again later.");
+          setError("An error occurred. Please try again later.");
         }
       } else if (error.request) {
-        // The request was made but no response was received
-        alert("No response from the server. Please try again later.");
+        setError("No response from the server. Please try again later.");
       } else {
-        // Something happened in setting up the request that triggered an error
         console.error("Error:", error.message);
-        alert("An error occurred. Please try again later.");
+        setError("An error occurred. Please try again later.");
       }
     }
   };
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-
-  //   if (
-  //     form.firstName === "" ||
-  //     form.lastName === "" ||
-  //     form.password === "" ||
-  //     form.confpassword === "" ||
-  //     form.mobileNo === "" ||
-  //     form.dob === "" ||
-  //     form.address === ""
-  //   ) {
-  //     alert("Please fill all the fields!");
-  //     return;
-  //   } else if (form
-  //     .mobileNo.match(/^[0-9]{10}$/) === null) {
-  //     alert("Please enter a valid mobile number!");
-  //     return;
-  //   }else if
-  //   //Validate password and confpassword
-  //   (form.password !== form.confpassword) {
-  //     alert("Password and confirm password do not match!");
-  //     return;
-  //   }
-
-  //   try {
-  //     const response = await axios.post(
-  //       "http://localhost:5000/server/registercaretaker",
-  //       form
-  //     );
-  //     console.log(response.data);
-  //     navigate("/Login");
-  //   } catch (error) {
-  //     console.error(error);
-  //     alert("Registration failed. Please try again!");
-  //   }
-  // };
 
   return (
     <>
@@ -130,6 +91,7 @@ const Register = () => {
                 <div className="title mb-4 text-center">
                   <h3>Register</h3>
                 </div>
+                {error && <p className="text-danger">{error}</p>} {/* Display error message */}
                 <Form onSubmit={handleSubmit}>
                   <Form.Group className="mb-4">
                     <Form.Label>First Name</Form.Label>
@@ -150,6 +112,15 @@ const Register = () => {
                     />
                   </Form.Group>
                   <Form.Group className="mb-4">
+                    <Form.Label>User Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter your user name"
+                      name="userName"
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-4">
                     <Form.Label>Password</Form.Label>
                     <Form.Control
                       type="password"
@@ -162,12 +133,11 @@ const Register = () => {
                     <Form.Label>Confirm Password</Form.Label>
                     <Form.Control
                       type="password"
-                      placeholder="Enter your password"
+                      placeholder="Enter your confirm password"
                       name="confPassword"
                       onChange={handleChange}
                     />
                   </Form.Group>
-
                   <Form.Group className="mb-4">
                     <Form.Label>Contact Number</Form.Label>
                     <Form.Control
@@ -187,11 +157,7 @@ const Register = () => {
                   </Form.Group>
                   <Form.Group className="mb-4">
                     <Form.Label>Gender</Form.Label>
-                    <Form.Control
-                      as="select"
-                      name="gender"
-                      onChange={handleChange}
-                    >
+                    <Form.Control as="select" name="gender" onChange={handleChange}>
                       <option value="">Select</option>
                       <option value="male">Male</option>
                       <option value="female">Female</option>
@@ -209,29 +175,16 @@ const Register = () => {
                   </Form.Group>
                   <Form.Group className="mb-4">
                     <Form.Label>Care Category</Form.Label>
-                    <Form.Control
-                      as="select"
-                      name="category"
-                      onChange={handleChange}
-                    >
+                    <Form.Control as="select" name="category" onChange={handleChange}>
                       <option value="">Select</option>
                       <option value="mental">MENTAL</option>
                       <option value="disable">DISABILITY</option>
                       <option value="elder">ELDERING</option>
                     </Form.Control>
                   </Form.Group>
-                  <Row>
-                    <Col md="12">
-                      <Button
-                        className="w-100 mb-4"
-                        size="md"
-                        variant="primary"
-                        type="submit"
-                      >
-                        Sign Up
-                      </Button>
-                    </Col>
-                  </Row>
+                  <Button className="w-100 mb-4" size="md" variant="primary" type="submit">
+                    Sign Up
+                  </Button>
                 </Form>
               </Card.Body>
             </Card>
@@ -243,4 +196,3 @@ const Register = () => {
 };
 
 export default Register;
-
