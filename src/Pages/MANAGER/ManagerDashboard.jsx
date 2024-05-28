ae7eacbc632ac4bb1d2ae68195019610c3c8e0c9
NavodYasara <111131809+NavodYasara@users.noreply.github.com>
2024-05-28 12:59:57 +0530

    nothing

--- src/Pages/MANAGER/ManagerDashboard.jsx
+++ src/Pages/MANAGER/ManagerDashboard.jsx
@@ -19,7 +19,6 @@ const ManagerDashboard = () => {
   const [caregivers, setCaregivers] = useState([]); // State for caregivers
 
   useEffect(() => {
-<<<<<<< HEAD
     // Fetch requirements
     fetch("http://localhost:5000/api/manager/getcaretakerInformation")
     
