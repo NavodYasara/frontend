8b254870d4e7c8bdb60d8c8370a576ad7566dfdd
Navod Yasara <navodyasara567@gmail.com>
2024-11-04 23:00:20 +0530

    hide unneccesery sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -103,59 +103,59 @@ export default Sidebar;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-import React from 'react';
-import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem,
-} from 'cdbreact';
-import { NavLink } from 'react-router-dom';
+// import React from 'react';
+// import {
+//   CDBSidebar,
+//   CDBSidebarContent,
+//   CDBSidebarFooter,
+//   CDBSidebarHeader,
+//   CDBSidebarMenu,
+//   CDBSidebarMenuItem,
+// } from 'cdbreact';
+// import { NavLink } from 'react-router-dom';
 
-const Sidebar = () => {
-  return (
-    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
-      <CDBSidebar textColor="#fff" backgroundColor="#333">
-        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-          <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-            Sidebar(Caretaker)
-          </a>
-        </CDBSidebarHeader>
+// const Sidebar = () => {
+//   return (
+//     <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
+//       <CDBSidebar textColor="#fff" backgroundColor="#333">
+//         <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+//           <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
+//             Sidebar(Caretaker)
+//           </a>
+//         </CDBSidebarHeader>
 
-        <CDBSidebarContent className="sidebar-content">
-          <CDBSidebarMenu>
-            <NavLink exact to="/CaretakerDashboard" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">Your Profile</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/Careplan " activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">View CarePlans</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/CaregiverDetail" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Caregiver details</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/Report" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">Reports</CDBSidebarMenuItem>
-            </NavLink>
-             <NavLink exact to="/Payment" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="exclamation-circle">Payment</CDBSidebarMenuItem>
-            </NavLink>
-          </CDBSidebarMenu>
-        </CDBSidebarContent>
+//         <CDBSidebarContent className="sidebar-content">
+//           <CDBSidebarMenu>
+//             <NavLink exact to="/CaretakerDashboard" activeClassName="activeClicked">
+//               <CDBSidebarMenuItem icon="columns">Your Profile</CDBSidebarMenuItem>
+//             </NavLink>
+//             <NavLink exact to="/Careplan " activeClassName="activeClicked">
+//               <CDBSidebarMenuItem icon="table">View CarePlans</CDBSidebarMenuItem>
+//             </NavLink>
+//             <NavLink exact to="/CaregiverDetail" activeClassName="activeClicked">
+//               <CDBSidebarMenuItem icon="user">Caregiver details</CDBSidebarMenuItem>
+//             </NavLink>
+//             <NavLink exact to="/Report" activeClassName="activeClicked">
+//               <CDBSidebarMenuItem icon="chart-line">Reports</CDBSidebarMenuItem>
+//             </NavLink>
+//              <NavLink exact to="/Payment" activeClassName="activeClicked">
+//               <CDBSidebarMenuItem icon="exclamation-circle">Payment</CDBSidebarMenuItem>
+//             </NavLink>
+//           </CDBSidebarMenu>
+//         </CDBSidebarContent>
 
-        <CDBSidebarFooter style={{ textAlign: 'center' }}>
-          <div
-            style={{
-              padding: '20px 5px',
-            }}
-          >
-            Sidebar Footer
-          </div>
-        </CDBSidebarFooter>
-      </CDBSidebar>
-    </div>
-  );
-};
+//         <CDBSidebarFooter style={{ textAlign: 'center' }}>
+//           <div
+//             style={{
+//               padding: '20px 5px',
+//             }}
+//           >
+//             Sidebar Footer
+//           </div>
+//         </CDBSidebarFooter>
+//       </CDBSidebar>
+//     </div>
+//   );
+// };
 
-export default Sidebar;
+// export default Sidebar;
